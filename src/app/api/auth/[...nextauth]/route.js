9a1d6d28bbeec04b98cb4b0aec28da4a3e7208a7
Riddha2004 <riddharc2004@gmail.com>
2024-05-18 22:10:14 +0530

    fixing api route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -41,7 +41,7 @@ export const authOptions = {
   ],  
 };
 
-export async function isAdmin() {
+async function isAdmin() {
   const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
   if (!userEmail) {
