78a125258506f1f0e84dd710c17ab7783c4e30c1
Riddha2004 <riddharc2004@gmail.com>
2024-05-18 19:29:58 +0530

    Updated Errors

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -17,7 +17,7 @@ export default function MenuPage() {
     return (
       <section className="mt-8">
         {categories?.length > 0 && categories.map(c=>(
-            <div kry={c._id}>
+            <div key={c._id}>
                 <div className="text-center">
                  <SectionHeaders mainHeader={c.name}/>
                 </div>
