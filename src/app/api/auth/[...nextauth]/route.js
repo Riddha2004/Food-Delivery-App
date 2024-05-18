da3ddc36e2a8b15e8978187548831a049f60fc68
Riddha2004 <128740445+Riddha2004@users.noreply.github.com>
2024-05-18 19:47:11 +0530

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -44,11 +44,11 @@ export const authOptions = {
 export async function isAdmin() {
   const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
-  if(!userEmail) {
+  if (!userEmail) {
     return false;
   }
   const userInfo = await UserInfo.findOne({email:userEmail});
-  if(!userInfo) {
+  if (!userInfo) {
     return false;
   }
   return userInfo.admin;
