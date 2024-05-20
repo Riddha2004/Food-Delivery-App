1c70433281ad0b666dbe83538e347340a1993dcc
Riddha2004 <riddharc2004@gmail.com>
2024-05-20 10:35:46 +0530

    fixing authOptions

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
 import clientPromise from "@/libs/mongoConnect";
+import {UserInfo} from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
-import {User} from "@/models/User";
-import NextAuth, { getServerSession } from "next-auth";
+import {User} from '@/models/User';
+import NextAuth, {getServerSession} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { UserInfo } from "@/models/UserInfo";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
   secret: "gchgjbdcjhbvfbkjnhvdkjskjd",
