0512f26936f53c5933e0d5ea9ac41ef81e52f78f
Riddha2004 <riddharc2004@gmail.com>
2024-05-20 10:59:56 +0530

    fixes

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,4 +1,4 @@
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/app/api/auth/[...nextauth]";
 import { User } from "@/models/User";
 import { UserInfo } from "@/models/UserInfo";
 import mongoose from "mongoose";
